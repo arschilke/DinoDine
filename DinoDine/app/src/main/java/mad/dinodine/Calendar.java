b05aed4b32d31d6878bb30f20d43ed3f579fd6f3
ElihuAxicor <elihudelvalle@hotmail.com>
2018-09-28 17:58:07 +1000

    Added calendar.xml to layout, added calendar_shape to drawables, added Calendar.java to java classes, added TableViewListView to manifest, added ListView to xml (unfinished so I'm sorry about the errors)

---
+++ DinoDine/app/src/main/java/mad/dinodine/Calendar.java
@@ -0,0 +1,32 @@
+package mad.dinodine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+    public class Calendar extends AppCompatActivity {
+
+        ImageButton checkButton;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.calendar_activity);
+
+            checkButton = findViewById(R.id.checkButton);
+
+            checkButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+
+                public void onClick(View v){
+
+                    //go to Select Time screen
+
+                }
+            });
+
+        }
+
+    }
