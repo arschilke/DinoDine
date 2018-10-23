614cdfcda74024cf5331d9f2ddc407a9bcd4c6eb
alyssaschilke <arschilke@loyola.edu>
2018-10-23 13:01:25 +1100

    booking view query

--- DinoDine/app/src/main/java/mad/dinodine/Allocation.java
+++ DinoDine/app/src/main/java/mad/dinodine/Allocation.java
@@ -7,13 +7,13 @@ import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
-@Entity(primaryKeys = {"booking","table"},
+@Entity(primaryKeys = {"booking","table_id"},
         foreignKeys = {@ForeignKey(entity = Booking.class, parentColumns = "booking_id", childColumns = "booking"),
-                       @ForeignKey(entity = Table.class, parentColumns = "table_id", childColumns = "table")},
-        indices = @Index(value = {"table"}))
+                @ForeignKey(entity = Table.class, parentColumns = "table_id", childColumns = "table_id")},
+        indices = @Index(value = {"table_id"}))
 public class Allocation {
 
-//todo add index for foreign keys...
+    //todo add index for foreign keys...
 // 'table column references a foreign key but it is not part of an index'
     @NonNull
     private String booking;
@@ -26,10 +26,10 @@ public class Allocation {
         this.table_id = "empty";
     }
 
-    public Allocation(String booking, String table){this.booking = booking;this.table_id=table;}
+    public Allocation(String booking, String table_id){this.booking = booking;this.table_id=table_id;}
 
     public String getBooking(){return booking;}
-    public String getTable(){return table_id;}
+    public String getTable_id(){return table_id;}
 
     public void setBooking(String booking){this.booking = booking;}
     public void setTable(String table){this.table_id = table;}
--- DinoDine/app/src/main/java/mad/dinodine/BookingViewActivity.java
+++ DinoDine/app/src/main/java/mad/dinodine/BookingViewActivity.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BookingViewActivity extends AppCompatActivity {
-
+    private AppRoomDB mDb;
     int tableID=-1;
     String tableName="";
     TextView title = null;
+    List<Allocation> alist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +28,13 @@ public class BookingViewActivity extends AppCompatActivity {
         title = findViewById(R.id.bookViewTitle);
         //title.setText("tableName: " + tableName + " tableID: " +tableID);
         title.setText("Viewing booking for table " + tableName.substring(1));
+
+        mDb = AppRoomDB.getInMemoryDatabase(getApplicationContext());
+
+        alist = mDb.allocationModel().getAllocationsForTable(tableName);
+
+
+
+
     }
 }
