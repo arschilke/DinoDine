e171526b57cc13dc08860a31d77705b80e0a1b3c
Amanda Tombolato <amandatombolato@gmail.com>
2018-10-24 00:36:53 +0000

    test toast message

--- DinoDine/app/src/main/java/mad/dinodine/SelectDateActivity.java
+++ DinoDine/app/src/main/java/mad/dinodine/SelectDateActivity.java
@@ -60,9 +60,9 @@ public class SelectDateActivity extends AppCompatActivity {
                 //set booked date to time of new calendar
                 if(book.getTimeInMillis() < rn.getTimeInMillis()) // if chosen date is less  than current date (before currentDate)
                 {Log.d("ERROR","BOOKED DATE IN PAST");
-                    Toast.makeText(getApplicationContext(),"NO GOOD",(int) 0).show();}
+                    Toast.makeText(getApplicationContext(),"INVALID DATE",(int) 0).show();}
                 else
-                    Toast.makeText(getApplicationContext(),"DATE IS GREAT",(int) 0).show();
+                    Toast.makeText(getApplicationContext(),"DATE IS FINE",(int) 0).show();
                 dateBooked = book.getTimeInMillis();
 
                 //set booking date.
